be66e81bac9c16f8db00a49536884dac20ce7654
Pradeep Singh Negi <pradeepnegi2012@gmail.com>
2023-09-04 16:10:44 +0530

    Time: 28 ms (71.62%) | Memory: 32.7 MB (17.38%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if (root == NULL)
+            return NULL;
+        if (root->val == key)
+            return helper(root);
+        TreeNode* curr = root;
+        while (curr != NULL) {
+            if (curr->val > key) {
+                if (curr->left != NULL && curr->left->val == key) {
+                    curr->left = helper(curr->left);
+                    break;
+                } else {
+                    curr = curr->left;
+                }
+            } else {
+                if (curr->right != NULL && curr->right->val == key) {
+                    curr->right = helper(curr->right);
+                    break;
+                } else {
+                    curr = curr->right;
+                }
+            }
+        }    
+        return root;
+    }
+
+    TreeNode* helper(TreeNode* root) {
+        if (root->left == NULL && root->right == NULL) 
+            return NULL;   
+        if (root->left == NULL)
+            return root->right;
+        if (root->right == NULL)
+            return root->left;
+        TreeNode* rightChild = root->right;
+        TreeNode* lastRight = findLastRight(root->left);
+        lastRight->right = rightChild;
+        return root->left;        
+    }
+
+    TreeNode* findLastRight(TreeNode* root) {
+        while (root->right != NULL)
+            root = root->right;
+        return root;   
+    }
+};
