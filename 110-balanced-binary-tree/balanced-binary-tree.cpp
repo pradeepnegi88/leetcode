7c589a4d8f54b2d74ffeaeb8a2d4f748b5962a56
Pradeep Singh Negi <pradeepnegi2012@gmail.com>
2023-09-04 16:26:33 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (12.77%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+int f(TreeNode* root){
+    if(root == NULL) return 0;
+     int left  = f(root->left);
+     if(left == -1) return -1;
+     int right  = f(root->right);
+     if(right == -1) return -1;
+     if(abs(left-right)>1) return -1;
+     return 1+max(left,right);
+
+}
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        // if(f(root) == -1){
+        //     return false;
+        // }else{
+        //     return true;
+        // }
+        stack<TreeNode*> s;
+                TreeNode* pre;
+
+        unordered_map<TreeNode*, int> m; 
+        TreeNode*  curr = root;
+         while(curr != NULL || !s.empty()){
+             if(curr!=NULL){
+                 s.push(curr);
+                 curr = curr->left;
+             }else{
+                 curr = s.top();
+        
+
+             if(curr->right!=NULL && curr->right!=pre){
+                  curr = curr->right;
+              }else{
+                   int left =0;
+                 int right =0;
+                 if(m.find(curr->left) != m.end()){ 
+                   left =  m[curr->left];
+                     }
+                  if(m.find(curr->right) != m.end()){
+                   right =   m[curr->right];
+                   }
+
+                if (abs(left - right) > 1) return false;
+                    m[curr] = 1 + max(left, right);                          
+                     s.pop();
+                  pre = curr;
+                  curr = NULL;
+              }
+             }
+         }
+         return true;
+    }
+};
+
+ 
