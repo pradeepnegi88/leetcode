1fc0aea685abdcf210b5e020e8427ff3a5dd69b7
Pradeep Singh Negi <pradeepnegi2012@gmail.com>
2024-05-21 21:36:46 +0530

    Time: 1186 ms (73.84%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -2,7 +2,7 @@
  
 select s.user_id,coalesce(ROUND(confirmation_rate,2),0.00) as confirmation_rate
 from  Signups as s
-left join(select user_id, sum(case when action = "confirmed" then 1 else 0 end) /count(*) as confirmation_rate
+left join(select user_id, avg(case when action = "confirmed" then 1 else 0 end) as confirmation_rate
 from Confirmations
 group by user_id) as g
 on s.user_id = g.user_id
