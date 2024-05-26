a2f48ba5687472eaa1fcf85ce31e31ca76076549
Pradeep Singh Negi <pradeepnegi2012@gmail.com>
2024-05-27 00:07:26 +0530

    Time: 598 ms (31.35%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,7 +1,14 @@
-SELECT
-    IFNULL(
-      (SELECT DISTINCT Salary
-       FROM Employee
-       ORDER BY Salary DESC
-        LIMIT 1 OFFSET 1),
-    NULL) AS SecondHighestSalary
+-- SELECT
+--     IFNULL(
+--       (SELECT DISTINCT Salary
+--        FROM Employee
+--        ORDER BY Salary DESC
+--         LIMIT 1 OFFSET 1),
+--     NULL) AS SecondHighestSalary
+
+WITH c AS (
+    SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS dr
+    FROM Employee
+)
+SELECT 
+    (SELECT salary FROM c WHERE dr = 2 limit 1) AS SecondHighestSalary
