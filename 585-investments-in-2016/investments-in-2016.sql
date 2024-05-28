d441271fc0b68d0bed6f0a4bb0ee8852af92aa74
Pradeep Singh Negi <pradeepnegi2012@gmail.com>
2024-05-28 21:41:20 +0530

    Time: 1518 ms (9.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+with ans as (select *,
+dense_rank() over( order by tiv_2015 ) as dr,
+count(tiv_2015) over( partition by tiv_2015 order by tiv_2015 ) as c,
+concat(lat,lon) as latlan,
+count(concat(lat,lon)) over( partition by lat,lon order by lat,lon ) as ll
+from Insurance)
+
+select round(sum(tiv_2016),2) as tiv_2016 from ans 
+where c>1 and ll=1
