c1c6db04b90f8c57aea22a51d79d5ac1b9acaa29
Pradeep Singh Negi <pradeepnegi2012@gmail.com>
2024-05-28 21:45:17 +0530

    Time: 1201 ms (24.99%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
 with ans as (select *,
-dense_rank() over( order by tiv_2015 ) as dr,
 count(tiv_2015) over( partition by tiv_2015 order by tiv_2015 ) as c,
 count(concat(lat,lon)) over( partition by lat,lon order by lat,lon ) as ll
 from Insurance)
